7851186ac14bb1b1e79eb3d339166184e3940bc6
Amandine <amandine.richaudeau@outlook.fr>
2018-12-04 13:56:27 +0100

    Commit Calculator XCode

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,11 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var uiLabelResult: UILabel!
+    var operatorSign: String?
+    var previousValue: Double = 0
+    var result: Double = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    @IBAction func OnDigitTap(_ sender: UIButton) {
+        
+        if uiLabelResult.text == "0"{
+        uiLabelResult.text?.append(sender.titleLabel?.text ?? "")
+    }
+    }
+    
+    @IBAction func OnOperatorTap(_ sender: UIButton) {
+        previousValue = Double(uiLabelResult.text ?? "0") ?? 0
+        operatorSign = sender.titleLabel?.text
+        uiLabelResult.text = "0"
+    }
+    
+    
+    @IBAction func OnCalculTap(_ sender: UIButton) {
+        switch operatorSign {
+        case "+":
+            result = previousValue + (Double(uiLabelResult.text ?? "0") ?? 0)
+        case "-":
+            result = previousValue - (Double(uiLabelResult.text ?? "0") ?? 0)
+        case "*":
+            result = previousValue * (Double(uiLabelResult.text ?? "0") ?? 0)
+        case "/":
+            result = previousValue / (Double(uiLabelResult.text ?? "0") ?? 0)
+        default :
+            print("erreur")
+        }
+        uiLabelResult.text = String(result)
+    }
+    
 
-
+    @IBAction func OnDeleteTap(_ sender: UIButton) {
+        uiLabelResult.text = "0"
+    }
 }
 
